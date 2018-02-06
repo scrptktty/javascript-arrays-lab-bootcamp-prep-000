80384199351b612f267f5bf10d229cb2731ecade
Kachel Wittig <kjwittig@gmail.com>
2018-02-06 11:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ function appendKitten(name) {
   return [name, ...kittens]
 }
 
-//Spread syntax allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.
-
-function prependKitten(name) {}
+function prependKitten(name) {
+  return [name, ...kittens]
+}
 
 function removeLastKitten() { 
   return kittens.pop()
