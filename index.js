4270e90e8a01207ef9581c6e83688c9c00ab34ef
Kachel Wittig <kjwittig@gmail.com>
2018-02-06 12:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,9 +40,6 @@ function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1)
 }
 
-//The slice() method returns a shallow copy of a portion of an array into a new array object selected from begin to end (end not included). The original array will not be modified.
-
-
 function removeFirstKitten() {
   return kitten.slice(0)
 }
